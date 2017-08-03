0f8c5b90d480f2da74522c376b68b9efb05faf28
rogerlevy <roger.levy@gmail.com>
2017-08-02 22:50:22 -0400

    DRAW-TILEMAP initial commit

--- mo/tilegame.f
+++ mo/tilegame.f
@@ -4,15 +4,45 @@
 \   to be doing a lot of stuff that would be inappropriate as tiles anyway.
 
 \ TODO:
-\  unloadtiles - destroy all subbitmaps and clear the cellstack
 \  tile collision
-\  tilemap? display and other routines...
+\  render a flipped tilemap
 
 bu: idiom tilegame:
-import bu/mo/image
-import bu/mo/cellstack
+    import bu/mo/image
+    import bu/mo/cellstack
+    import bu/mo/a
+    import bu/mo/pen
+    import bu/mo/draw
 
-4096 cellstack tiles
-: tile  4095 and tiles [] @ ;
+16384 16 * dup constant maxtiles  cellstack tiles
+: tile  maxtiles 1 - and tiles [] @ ;
 : +tiles  tiles @length swap  dup subcount @ 0 do  i over imgsubbmp  tiles push  loop  drop ;
-: loadtiles ( image tilew tileh -- firstn ) third subdivide  +tiles ;
+: add-tiles ( image tilew tileh -- firstn ) third subdivide  +tiles ;
+: change-tiles  ( image tilew tileh n -- )  tiles !length  add-tiles  drop ;
+: clear-tiles  ( -- )  0 tiles [] a!>  maxtiles for  @+ -bmp  loop  tiles vacate ;
+
+
+\ Render a tilemap
+\  Given a starting address, a pitch, and a base tile index, render tiles to fill the current
+\  clip rectangle of the current destination bitmap.
+\  The tiles are cell-sized and in the following format:
+\  00vh 0000 0000 bbbb tttt tttt tttt tt00  ( t=tile # 0-16383, b=bank 0-15, v=vflip, h=hflip)
+\  DRAW-TILEMAP draws within the clip rectangle
+\  DRAW-TILEMAP-BG draws within the clip rectangle plus 1 tile in both directions, enabling scrolling
+\  NOTE: The base tile's width and height defines the "grid dimensions".
+
+: (draw-tilemap)  ( addr #pitch rows cols basetile -- )
+    cells 0 tiles [] +  dup @  bmpwh  locals| th tw tba cols rows pitch |  cells +
+    a@ >r
+    rows for
+        at@  ( addr x y )
+        third a!  cols for  @+ dup $000ffffc and tba + @  swap 28 >>  blitf  tw 0 +at  loop
+        th + at   pitch +
+    loop  drop  r> a! ;
+
+: draw-tilemap  ( addr #pitch basetile -- )
+    >r  clipxy clipwh 2+  third subw 2v@ 2/  r>  (draw-tilemap) ;
+
+: draw-tilemap-bg  ( addr #pitch basetile -- )
+    >r  clipxy clipwh 2+  third subw 2v@ 2/  1 1 2+  r>  (draw-tilemap) ;
+
